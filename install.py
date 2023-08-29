3bf1d22648f1fd8823a8447c6d520d159bdc5cef
Milan Staffehl <milan.staffehl@stud.uni-heidelberg.de>
2023-08-29 10:45:17 +0200

    Add milestone 001 specific install instructions

--- install.py
+++ install.py
@@ -18,10 +18,12 @@ def install():
     figures_home = root / "figures"
 
     # create directories
+    print("Setting up top-level directories.")
     for directory in [data_home, external, figures_home]:
         directory.mkdir()
 
     # create subdirs
+    print("Setting up subdirectories for milestones and simulations.")
     for i in range(1, MILESTONES + 1, 1):
         milestone_dir = f"{i:03d}"
         (figures_home / milestone_dir).mkdir()
@@ -30,6 +32,20 @@ def install():
             sim_dir = simulation.replace("-", "_")
             (figures_home / milestone_dir / sim_dir).mkdir()
             (data_home / milestone_dir / sim_dir).mkdir()
+
+    # milestone-specific set-up
+    _setup_milestone_001(figures_home / "001")
+
+
+def _setup_milestone_001(milestone_figures_home):
+    """
+    Set up directories for the 001 milestone.
+    """
+    print("Setting up subdirectories for milestone 001.")
+    for sim_dir in milestone_figures_home:
+        (sim_dir / "galleries").mkdir()
+        (sim_dir / "hists").mkdir()
+        (sim_dir / "normalized").mkdir()
 
 
 if __name__ == "__main__":
