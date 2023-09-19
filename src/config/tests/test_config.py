5135f0078c07c764fc5624e8974f0e385854e528
Milan Staffehl <milan.staffehl@stud.uni-heidelberg.de>
2023-09-19 14:08:09 +0200

    Dummy commit

--- src/config/tests/test_config.py
+++ src/config/tests/test_config.py
@@ -1,12 +1,17 @@
 """
 Test the config module.
 """
+import pytest
 from pathlib import Path
 
 from config import config
 
 
-def test_default_config():
+@pytest.mark.skipif(
+    not Path("/virgotng/").exists(), 
+    reason="Cannot be executed outside of the Vera cluster."
+)
+def test_default_config_vera(mocker):
     """
     Test the default config returned by get_default_config.
     """
@@ -21,14 +26,51 @@ def test_default_config():
     assert Path(test_cfg.figures_home).resolve() == root_dir / "figures"
 
 
-def test_custom_config():
+def test_default_config_generic(mocker):
+    """
+    Test the default config returned by get_default_config.
+    """
+    # set global vars to some path
+    mock_config = {
+        "paths":
+            {
+                "data_home": str(Path(__file__).parents[3].resolve() / "data"),
+                "figures_home": str(Path(__file__).parents[3].resolve() / "figures"),
+                "simulation_home": str(Path().home() / ".local"),
+            }
+    }  # yapf: disable
+    mock_load = mocker.patch("yaml.full_load")
+    mock_load.return_value = mock_config
+    test_cfg = config.get_default_config("TNG300-1")
+    sim_path = str(Path().home() / ".local" / "TNG300-1" / "output")
+    assert test_cfg.base_path == sim_path
+    assert test_cfg.snap_num == 99
+    assert test_cfg.mass_field == "Group_M_Crit200"
+    assert test_cfg.radius_field == "Group_R_Crit200"
+    root_dir = Path(__file__).parents[3].resolve()
+    assert Path(test_cfg.data_home).resolve() == root_dir / "data"
+    assert Path(test_cfg.figures_home).resolve() == root_dir / "figures"
+
+
+def test_custom_config(mocker):
     """
     Test the config received when specifying parameters.
     """
+    # set global vars to some path
+    mock_config = {
+        "paths":
+            {
+                "data_home": str(Path(__file__).parents[3].resolve() / "data"),
+                "figures_home": str(Path(__file__).parents[3].resolve() / "figures"),
+                "simulation_home": str(Path().home() / ".local"),
+            }
+    }  # yapf: disable
+    mock_load = mocker.patch("yaml.full_load")
+    mock_load.return_value = mock_config
     test_cfg = config.get_default_config(
         "TNG50-2", 50, "Group_M_Crit500", "Radius"
     )
-    sim_path = "/virgotng/universe/IllustrisTNG/TNG50-2/output"
+    sim_path = str(Path().home() / ".local" / "TNG50-2" / "output")
     assert test_cfg.base_path == sim_path
     assert test_cfg.snap_num == 50
     assert test_cfg.mass_field == "Group_M_Crit500"
