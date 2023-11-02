9ae56193834f3ba52d7c0d277b9452bc2f7d0374
Milan Staffehl <milan.staffehl@stud.uni-heidelberg.de>
2023-11-02 10:50:57 +0100

    Change color for hot gas (preliminary choice)
    Closes #61

--- src/library/plotting/mass_trends.py
+++ src/library/plotting/mass_trends.py
@@ -17,52 +17,6 @@ if TYPE_CHECKING:
     from numpy.typing import NDArray
 
 
-def plot_gas_mass_trends(
-    halo_masses: NDArray,
-    cool_gas: NDArray,
-    warm_gas: NDArray,
-    hot_gas: NDArray,
-    cool_gas_err: NDArray,
-    warm_gas_err: NDArray,
-    hot_gas_err: NDArray
-) -> tuple[Figure, Axes]:
-    """
-    Plot the mass trends of gas mass and gas fraction with halo mass.
-
-    All gas data arrays are expected to have shape (2, 2, M) where
-    the first axis differentiates the weight type (fraction and mass),
-    the second axis the data type (mean and median) and the third
-    axis the mass bins, i.e. the values. The individual values are
-    either the gas fraction of the corresponding regime in that mass
-    bin or the total gas mass in units of solar masses.
-
-    Similarly, the error arrays are of shape (2, 2, M), where the
-    first axis again holds the weight type, and the second axis the
-    lower and upper error values.
-
-    Function does not save the figure to file but returns it.
-
-    :param halo_masses: Array of shape (M, ) holding the halo masses
-        in units of solar masses.
-    :param cool_gas: Cool gas data, shape (2, 2, M)
-    :param warm_gas: Warm gas data, shape (2, 2, M)
-    :param hot_gas: Hot gas data, shape (2, 2, M)
-    :param cool_gas_err: Cool gas errors, shape (2, 2, M)
-    :param warm_gas_err: Warm gas errors, shape (2, 2, M)
-    :param hot_gas_err: Hot gas errors, shape (2, 2, M)
-    :return: Figure and axes objects after being created.
-    """
-    raise NotImplementedError("Not implemented yet.")
-    # fig, axes = plt.subplots(nrows=2, figsize=(3.5, 6), sharex=True)
-    # axes[1].set_xlabel(r"Halo mass [$\log M_\odot$]")
-    # axes[0].set_ylabel("Gas fraction")
-    # axes[0].set_yscale("log")
-    # axes[1].set_ylabel(r"Gas mass [$M_\odot$]")
-    # axes[1].set_yscale("log")
-
-    # return fig, axes
-
-
 def plot_gas_mass_trends_individuals(
     halo_masses: NDArray,
     gas_data: NDArray,
@@ -138,7 +92,7 @@ def plot_gas_mass_trends_individuals(
     colors = {
         "cool": (0, 0, 205),  # mediumblue
         "warm": (128, 0, 128),  # purple
-        "hot": (255, 140, 0),  # darkorange
+        "hot": (210, 4, 45),  # cherry
     }
 
     # font size
