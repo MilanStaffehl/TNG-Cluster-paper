Fix wrong normalization of radial profile stacks

src/library/loading/load_radial_profiles.py
@@ -155,7 +155,7 @@ def load_individuals_2d_profile(
             logging.warning(f"Skipping non-file entry {filename}.")
         with np.load(filename) as data_file:
             histogram = data_file["histogram"]
-            # original_histogram = data_file["original_histogram"]  # unused
+            original_histogram = data_file["original_histogram"]
             xedges = data_file["xedges"]
             yedges = data_file["yedges"]
             halo_id = data_file["halo_id"]
@@ -164,6 +164,7 @@ def load_individuals_2d_profile(
         # construct dictionary
         halo_data = {
             "histogram": histogram,
+            "original_histogram": original_histogram,
             "xedges": xedges,
             "yedges": yedges,
             "halo_id": halo_id,

src/library/processing/statistics.py
@@ -321,8 +321,8 @@ def get_binned_medians(
 
 def column_normalized_hist2d(
     x: NDArray,
-    y: NDArray,
-    bins: int | tuple[int, int] | NDArray | tuple[NDArray, NDArray],
+    y: NDArray | None,
+    bins: int | tuple[int, int] | NDArray | tuple[NDArray, NDArray] | None,
     values: NDArray | None = None,
     ranges: NDArray | None = None,
     statistic: str = "sum",
@@ -348,24 +348,42 @@ def column_normalized_hist2d(
       not assign the smallest value in the column to zero; it merely
       normalizes every value to the largest one in the column.
 
-    The function also supports different types of bin statistics. The
-    options are identical to those of ``scipy.stats.binned_statistics_2d``,
-    see the`scipy documentation`_ for details. In order to get a normal
-    weighted histogram, use the weights as values and set "statistics"
-    to ``"sum"``. In order to get a count histogram, leave both the
-    values and the statistics arguments at their default values.
+    Alternatively, it is possible to give this function an existing 2D
+    histogram in the ``x`` parameter. In such a case, the ``y`` and
+    ``bins`` parameters **must** be set to None. If an existing 2D
+    histogram is supplied this way, it will be column normalized directly
+    instead of being created first. In this case, the parameters
+    ``values``, ``ranges``, and ``statistics`` have no effect and are
+    ignored.
 
-    .. attention:: The histogram will have shape (ny, nx) - contrary to
-        the standard order of most histogram generating functions! This
-        is done to ensure that the array will have the more intuitive,
-        readily understandable form wherein the first index selects a
-        row, and the second index selects a column (i.e. an entry of the
-        selected row). This shape is also expected for plotting functions
-        such as ``mtplotlib.pyplot.imshow``. To return to the original
-        shape, simply transpose the histogram array: ``hist.transpose()``.
+    The function also supports different types of bin statistics for new
+    histograms (i.e. when given sets of x- and y-values). The options are
+    identical to those of ``scipy.stats.binned_statistics_2d``, see the
+    `scipy documentation`_ for details. In order to get a normal weighted
+    histogram, use the weights as values and set "statistics" to
+    ``"sum"``. In order to get a count histogram, leave both the values
+    and the statistics arguments at their default values.
+
+    .. attention:: The returned histogram will have shape (ny, nx) --
+        contrary to the standard order of most histogram generating
+        functions! This is done to ensure that the array will have the
+        more intuitive, readily understandable form wherein the first
+        index selects a row, and the second index selects a column (i.e.
+        an entry of the selected row). This shape is also expected for
+        plotting functions such as ``matplotlib.pyplot.imshow``. To
+        return to the original shape as ``np.histogram2d`` would return,
+        simply transpose the histogram array:
+        ``column_normalized_hist2d(...).transpose()``.
 
     :param x: The array of shape (N, ) of x-positions of the data points.
+        Alternatively, this may be an existing 2D histogram which is to
+        be column-wise normalized. In such a case, x must be a 2D array
+        and y must be None. The histogram is expected to be of shape
+        (nx, ny). This is the order that numpy automatically produces in
+        ``np.histogram2d``.
     :param y: The array of shape (N, ) of y-positions of the data points.
+        Must be set to None if an existing 2D histogram is passed to the
+        x parameter, otherwise an exception will be raised.
     :param bins: The bins for the histogram. Can be one of the following:
         - int: In this case, both dimensions will be split into this
           number of bins.
@@ -375,6 +393,8 @@ def column_normalized_hist2d(
           dimensions.
         - tuple[NDArray, NDArray]: First array will specify the bin edges
           along the x-axis, the second the bin edges along the y-axis.
+        - None: Use this only when supplying an existing histogram to the
+          ``x`` parameter of this function.
     :param values: The values belonging to each data point. Must be of
         shape (N, ). Optional, leave empty for a simple count statistic.
         Defaults to None, which means it will automatically be replaced
@@ -387,30 +407,52 @@ def column_normalized_hist2d(
     :param normalization: The normalization to use along the columns.
         Choices are "density" or "range". Density normalization will
         normalize every column such that its values add up to one, while
-        schoosing range will normalize every column to its maximum value,
+        choosing range will normalize every column to its maximum value,
         such that every column will have 1 as its maximum value. Defaults
         to "density".
-    :raises RuntimeError: If an unsupported normalization is given.
-    :return: The tuple of the histogram, the x-edges and the y-edges.
-        The histogram is column-wise normalized according to the chosen
-        method.
+    :raises RuntimeError: If an unsupported normalization is given; or
+        if the supplied arguments for x, y and bins cannot be properly
+        parsed (i.e. if either y or bins is None, but the other is not
+        or when x has the wrong shape to be an existing 2D histogram).
+    :return: The tuple of the histogram of shape (ny, nx) (careful: this
+        is different from what you might expect; see not above), the
+        x-edges and the y-edges. The histogram is column-wise normalized
+        according to the chosen method.
+        If ``x`` was an existing 2D histogram, the x- and y-edges are
+        None instead as they cannot be inferred from only an existing
+        2D histogram alone. In such a case, the edges should already be
+        available from the generating function of the existing histogram
+        anyway.
 
     .. _scipy documentation: https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.binned_statistic_2d.html
     """  # noqa: B950
-    if x.shape != y.shape:
+    if y is None and bins is None and len(x.shape) == 2:
+        logging.debug(
+            "Received an existing histogram, will normalize it directly."
+        )
+        # expects hist as shape (nx, ny), so transposition is necessary
+        hist = x
+        xedges, yedges = None, None
+    elif x.shape != y.shape:
         logging.error(
             f"Received x and y data arrays of different shape: shape of x is "
             f"{x.shape} but y has shape {y.shape}."
         )
         return
-    # if no values are given, assume a normal count/sum is desired
-    if values is None:
-        values = np.ones_like(x)
+    else:
+        if bins is None or y is None:
+            raise RuntimeError(
+                "Input parameters for x, y and bins cannot be parsed:\n"
+                f"x: {x}\ny: {y}\nbins: {bins}\n"
+            )
+        # if no values are given, assume a normal count/sum is desired
+        if values is None:
+            values = np.ones_like(x)
 
-    # calculate histogram
-    hist, xedges, yedges, _ = scipy.stats.binned_statistic_2d(
-        x, y, values, statistic, bins, ranges
-    )
+        # calculate histogram
+        hist, xedges, yedges, _ = scipy.stats.binned_statistic_2d(
+            x, y, values, statistic, bins, ranges
+        )
 
     # normalize every column according to chosen normalization
     if normalization == "density":

src/library/processing/tests/test_statistics.py
@@ -3,6 +3,7 @@ Unit tests for the statistics module.
 """
 import numpy as np
 import numpy.ma as ma
+import pytest
 
 from library.processing import selection, statistics
 
@@ -129,12 +130,19 @@ def test_mask_quantity_uncompressed_dimensions():
     assert output.shape == (3, 2)
 
 
-def test_column_normalized_hist2d_density():
+@pytest.fixture
+def hist_data():
+    """Yield x- and y-data for a simple 2D histogram."""
+    x_data = np.array([1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3])
+    y_data = np.array([1, 2, 2, 2, 3, 3, 1, 1, 1, 1, 1, 1, 3, 3, 2, 3])
+    yield x_data, y_data
+
+
+def test_column_normalized_hist2d_density(hist_data):
     """
     Test that the function can return a simple count-based hist2d.
     """
-    x_data = np.array([1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3])
-    y_data = np.array([1, 2, 2, 2, 3, 3, 1, 1, 1, 1, 1, 1, 3, 3, 2, 3])
+    x_data, y_data = hist_data
     output = statistics.column_normalized_hist2d(x_data, y_data, (3, 3))
     assert output[0].shape == (3, 3)
     expected = np.array(
@@ -143,15 +151,44 @@ def test_column_normalized_hist2d_density():
     np.testing.assert_almost_equal(output[0], expected, decimal=2)
 
 
-def test_column_normalized_hist2d_range():
+def test_column_normalized_hist2d_range(hist_data):
     """
     Test the function using the range normalization.
     """
-    x_data = np.array([1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3])
-    y_data = np.array([1, 2, 2, 2, 3, 3, 1, 1, 1, 1, 1, 1, 3, 3, 2, 3])
+    x_data, y_data = hist_data
     output = statistics.column_normalized_hist2d(
         x_data, y_data, (3, 3), normalization="range"
     )
     assert output[0].shape == (3, 3)
+    expected = np.array([[1 / 3, 1, 0], [1, 0, 1], [2 / 3, 1 / 3, 1]])
+    np.testing.assert_almost_equal(output[0], expected, decimal=2)
+
+
+def test_column_normalized_hist2d_existing_histogram():
+    """
+    Test the function using an existing histogram to normalize.
+    """
+    # histogram is in shape (ny, nx) - this should be reproduced in result!
+    histogram = np.array(
+        [[1, 6, 0],
+         [3, 0, 1],
+         [2, 2, 1]]
+    )  # yapf: disable
+
+    # density
+    output = statistics.column_normalized_hist2d(histogram, None, None)
+    assert output[0].shape == (3, 3)
+    assert output[1] is None and output[2] is None
+    expected = np.array(
+        [[1 / 6, 3 / 4, 0], [1 / 2, 0, 1 / 2], [1 / 3, 1 / 4, 1 / 2]]
+    )
+    np.testing.assert_almost_equal(output[0], expected, decimal=2)
+
+    # range
+    output = statistics.column_normalized_hist2d(
+        histogram, None, None, normalization="range"
+    )
+    assert output[0].shape == (3, 3)
+    assert output[1] is None and output[2] is None
     expected = np.array([[1 / 3, 1, 0], [1, 0, 1], [2 / 3, 1 / 3, 1]])
     np.testing.assert_almost_equal(output[0], expected, decimal=2)

src/pipelines/radial_profiles/profiles_stacked.py
@@ -53,8 +53,8 @@ class StackProfilesPipeline(Pipeline):
         n_halos = len(files)
         # test-load first halo
         with np.load(files[0].resolve()) as data_file:
-            hist_shape = data_file["histogram"].shape
             if self.what == "temperature":
+                hist_attr = "original_histogram"
                 edges = [
                     data_file["xedges"][0],
                     data_file["xedges"][-1],
@@ -62,7 +62,9 @@ class StackProfilesPipeline(Pipeline):
                     data_file["yedges"][-1],
                 ]
             else:
+                hist_attr = "histogram"
                 edges = data_file["edges"]
+            hist_shape = data_file[hist_attr].shape
 
         # allocate memory
         histograms = np.zeros((n_halos, ) + hist_shape)
@@ -74,12 +76,23 @@ class StackProfilesPipeline(Pipeline):
             loader = load_radial_profiles.load_individuals_1d_profile
         # iterate through directory
         for i, halo_data in enumerate(loader(self.paths["data_dir"], hist_shape)):
-            histograms[i] = halo_data["histogram"]
+            histograms[i] = halo_data[hist_attr]
 
         # Step 3: stack the histograms
         stack = statistics.stack_histograms(histograms, method=self.method)
+        if self.what == "temperature":
+            # 2D histograms need to be normalized column-wise
+            normalized_histograms, _, _ = statistics.column_normalized_hist2d(
+                stack[0], None, None, normalization="density"
+            )
+            # re-assign to variable
+            stack = (
+                normalized_histograms,
+                stack[1].transpose(),
+                stack[2].transpose()
+            )
 
-        # Step 4: plot the results
+        # Step 5: plot the results
         if self.what == "temperature" and self.method == "mean":
             f, a = self._plot_2d_mean(stack, edges)
         elif self.what == "temperature" and self.method == "median":
@@ -98,7 +111,7 @@ class StackProfilesPipeline(Pipeline):
         # save plot to file
         if self.no_plots:
             return 0
-        name = f"{self.paths['figures_file_stem']}_{self.method}.pdf"
+        name = f"{self.paths['figures_file_stem']}_{self.method}.png"
         path = Path(self.paths["figures_dir"])
         if not path.exists():
             logging.info("Creating missing figures directory for stacks.")
@@ -118,7 +131,7 @@ class StackProfilesPipeline(Pipeline):
         :return: Tuple of figure and axes objects with the plots.
         """
         # create and configure figure and axes
-        fig, axes = plt.subplots(ncols=2, figsize=(10, 4), sharey=True)
+        fig, axes = plt.subplots(ncols=2, figsize=(9, 4), sharey=True)
         fig.set_tight_layout(True)
 
         # plot the mean/median
@@ -128,7 +141,7 @@ class StackProfilesPipeline(Pipeline):
             stack_data[0],
             edges,
             title="Mean temperature profile",
-            cbar_label="Mean gas fraction per radial shell",
+            cbar_label="Normalized gas mass fraction",
             scale="log" if self.log else "linear",
             log_msg="all halos above 10^14 M_sol",
         )