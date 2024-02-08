d69b0822e31e03611d1a89d4e189ca43c480ea61
Milan Staffehl <milan.staffehl@stud.uni-heidelberg.de>
2024-02-08 11:28:45 +0100

    Update install script to use paths from config.yaml

--- install.py
+++ install.py
@@ -6,17 +6,25 @@ def install():
     """
     Installs the project by creating the required directory structure.
     """
-    # cur dir (equals to root dir)
-    root = Path(__file__).parent.resolve()
-    # top level dirs
-    data_home = root / "data"
-    external = root / "external"
-    figures_home = root / "figures"
+    root_dir = Path(__file__).parent.resolve()
+    with open(root_dir / "config.yaml", "r") as config_file:
+        lines = config_file.readlines()
+    for line in lines:
+        line.lstrip()
+        if line.startswith("data_home"):
+            data_home = Path(line.removeprefix("data_home: ")).expanduser()
+        elif line.startswith("figures_home"):
+            figures_home = Path(line.removeprefix("figures_home: ")
+                                ).expanduser()
+
+    external = root_dir / "external"
 
     # create directories
     print("Setting up top-level directories.")
     for directory in [data_home, external, figures_home]:
-        directory.mkdir()
+        if not directory.exists():
+            print(f"Creating missing directory:{str(directory)}")
+            directory.mkdir(parents=True)
 
 
 if __name__ == "__main__":
