Fix missing peculiar velocity in radial velocity calculation

src/library/compute.py
@@ -121,30 +121,37 @@ def get_virial_temperature_only_mass(mass: float | NDArray) -> NDArray:
 
 
 def get_radial_velocities(
-    center: NDArray, positions: NDArray, velocities: NDArray
+    center: NDArray,
+    halo_velocity: NDArray,
+    positions: NDArray,
+    velocities: NDArray,
 ) -> NDArray:
     """
     Calculate the radial velocities in direction of ``center``.
 
     The returned array is the radial velocities. Positive value denote
-    velocities towards the center, negative velocities denote velocities
-    away from the center.
+    velocities away from the center (outflowing), negative velocities
+    denote velocities towards the center (infalling).
 
     :param center: Position vector of the center of the sphere, shape
         (3, ).
+    :param halo_velocity: Velocity vector of the halo center, shape
+        (3, 0). Must be in units of km/s. Set this to ``[0, 0, 0]`` if
+        the velocities are already w.r.t. the halo center.
     :param positions: Array of position vectors of the objects whose
         radial velocity to find. Shape (N, 3).
     :param velocities: Array of velocity vectors of the objects for
-        which to find the radial velocity w.r.t. center. Shape (N, 3).
+        which to find the radial velocity. Shape (N, 3).
     :return: Array of shape (N, ) of velocity components of the given
         velocities in direction of the center. Positive values denote
-        velocity towards center, negative values denote velocity away
-        from center. In units of km/s.
+        velocity away from the halo center, negative values denote
+        velocity towards the halo center. In units of km/s.
     """
+    relative_vel = velocities - halo_velocity
     radial_vectors = positions - center
     norms = np.linalg.norm(radial_vectors, axis=1)
     unit_vectors = np.divide(radial_vectors, norms[:, np.newaxis])
-    return np.sum(velocities * unit_vectors, axis=1)  # pair-wise dot product
+    return np.sum(relative_vel * unit_vectors, axis=1)  # pair-wise dot product
 
 
 def get_virial_velocity(

src/library/tests/test_compute.py
@@ -91,7 +91,19 @@ def test_compute_get_radial_velocities():
     test_center = np.array([1, 2])
     pos = np.array([[2, 2], [2, 3], [2, 1], [1, 1], [1, 3]])
     vel = np.array([[0, 1], [1, 1], [-1, 1], [1, -1], [1, -1]])
-    expected = np.array([0, np.sqrt(2), -np.sqrt(2), 1, -1])
 
-    output = compute.get_radial_velocities(test_center, pos, vel)
+    # velocities are w.r.t. halo center
+    test_group_vel = np.array([0, 0])
+    expected = np.array([0, np.sqrt(2), -np.sqrt(2), 1, -1])
+    output = compute.get_radial_velocities(
+        test_center, test_group_vel, pos, vel
+    )
+    np.testing.assert_allclose(output, expected, rtol=1e-4)
+
+    # halo has its own peculiar velocity
+    test_group_vel = np.array([1, -1])
+    expected = np.array([-1, np.sqrt(2), -2 * np.sqrt(2), 0, 0])
+    output = compute.get_radial_velocities(
+        test_center, test_group_vel, pos, vel
+    )
     np.testing.assert_allclose(output, expected, rtol=1e-4)

src/library/units.py
@@ -3,6 +3,7 @@ Utilities for unit conversion.
 """
 from typing import ClassVar, TypeVar
 
+import numpy as np
 from numpy.typing import NDArray
 
 from library import constants
@@ -55,6 +56,7 @@ class UnitConverter:
                 "Coordinates",
             ],  # noqa: E123
         "velocityLike": ["Velocities"],
+        "groupVelocityLike": ["GroupVel"],
         "sfrLike": ["GroupSFR", "StarFormationRate", "SubhaloSFR"],
         "massFlowLike": ["GroupBHMdot", "BH_Mdot", "BH_MdotEddington"],
         "energyLike": ["BH_CumEgyInjection_QM", "BH_CumEgyInjection_RM"],
@@ -89,12 +91,13 @@ class UnitConverter:
         return supported
 
     @classmethod
-    def convert(cls, quantity: N, field: str) -> N:
+    def convert(cls, quantity: N, field: str, snap_num: int = 99) -> N:
         """
         Automatically convert the quantity into physical units.
 
         :param quantity: Quantitiy in code units.
         :param field: Name of the field of the quantity.
+        :param snap_num: Number of the snapshot in which unit is located.
         :raises UnsupportedUnitError: When the field has no supported
             unit conversion available.
         :return: Quantity in physical units.
@@ -104,7 +107,9 @@ class UnitConverter:
         elif field in cls.fields["distanceLike"]:
             return cls.convert_distancelike(quantity)
         elif field in cls.fields["velocityLike"]:
-            return quantity
+            return cls.convert_velocitylike(quantity, snap_num)
+        elif field in cls.fields["groupVelocityLike"]:
+            return cls.convert_groupvelocitylike(quantity, snap_num)
         elif field in cls.fields["massFlowLike"]:
             return cls.convert_massflowlike(quantity)
         elif field in cls.fields["sfrLike"]:
@@ -137,6 +142,30 @@ class UnitConverter:
         :return: Distance-like quantity in ckpc.
         """
         return quantity / constants.HUBBLE
+
+    @staticmethod
+    def convert_velocitylike(quantity: N, snap_num: int) -> N:
+        """
+        Return the velocity-like quantity in km/s.
+
+        :param quantity: Velocity-like quantity in km/s * sqrt(a).
+        :param snap_num: The snapshot number.
+        :return: Velocity-like quantity in km/s.
+        """
+        a = 1 / (1 + constants.REDSHIFTS[snap_num])
+        return quantity * np.sqrt(a)
+
+    @staticmethod
+    def convert_groupvelocitylike(quantity: N, snap_num: int) -> N:
+        """
+        Return the group-velocity-like quantity in km/s.
+
+        :param quantity: Velocity-like quantity in km/s/sqrt(a).
+        :param snap_num: The snapshot number.
+        :return: Group-velocity-like in km/s.
+        """
+        a = 1 / (1 + constants.REDSHIFTS[snap_num])
+        return quantity / a
 
     @staticmethod
     def convert_massflowlike(quantity: N) -> N: