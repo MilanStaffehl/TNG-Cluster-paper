dc958e67d8e90eff851e223db16b424b30eaad0e
Milan Staffehl <milan.staffehl@stud.uni-heidelberg.de>
2024-08-22 11:26:44 +0200

    Add ridgeline plot to tracer plots

--- src/pipelines/tracer_history/simple_quantities.py
+++ src/pipelines/tracer_history/simple_quantities.py
@@ -5,14 +5,19 @@ from __future__ import annotations
 
 import dataclasses
 import logging
+from pathlib import Path
 from typing import TYPE_CHECKING, ClassVar
 
 import h5py
+import matplotlib.cm
+import matplotlib.colors
 import matplotlib.pyplot as plt
 import numpy as np
+import yaml
 
 from library import constants
 from library.plotting import common
+from library.processing import statistics
 from pipelines import base
 
 if TYPE_CHECKING:
@@ -30,6 +35,7 @@ class PlotSimpleQuantityWithTimePipeline(base.Pipeline):
 
     n_clusters: ClassVar[int] = 352
     n_snaps: ClassVar[int] = 100 - constants.MIN_SNAP
+    n_bins: ClassVar[int] = 50  # number of bins
 
     def run(self) -> int:
         """Load and plot data"""
@@ -41,17 +47,36 @@ class PlotSimpleQuantityWithTimePipeline(base.Pipeline):
             )
             return 1
 
-        # Step 1: allocate memory for quantities to trace
+        # Step 1: Find plot config
+        cfg = Path(__file__).parent / "simple_quantities_plot_config.yaml"
+        with open(cfg, "r") as cfg_file:
+            stream = cfg_file.read()
+        try:
+            plot_config = yaml.full_load(stream)[self.quantity]
+            hist_range = plot_config["min"], plot_config["max"]
+            log = plot_config["log"]
+        except KeyError:
+            logging.warning(
+                f"Found no plot config for quantity {self.quantity}, will set "
+                f"no boundaries for histograms."
+            )
+            hist_range = None
+            log = False
+
+        # Step 2: allocate memory for quantities to trace
         quantity_mean = np.zeros((self.n_clusters, self.n_snaps))
         quantity_median = np.zeros_like(quantity_mean)
         quantity_min = np.zeros_like(quantity_mean)
         quantity_max = np.zeros_like(quantity_mean)
-        # TODO: allocate memory for 2D hists
+        quantity_hists = np.zeros((self.n_clusters, self.n_snaps, self.n_bins))
 
-        # Step 2: open the archive
+        # Step 3: open the archive
         f = h5py.File(self.config.cool_gas_history, "r")
 
-        # Step 3: load quantity and process for plotting
+        # Step 4: load quantity and process for plotting
+        logging.info(
+            "Loading particle data from file and processing. May take a while."
+        )
         for zoom_id in range(self.n_clusters):
             logging.debug(
                 f"Loading and processing {self.quantity} for zoom-in "
@@ -80,17 +105,34 @@ class PlotSimpleQuantityWithTimePipeline(base.Pipeline):
             quantity_min[zoom_id] = np.nanmin(
                 quantity[constants.MIN_SNAP:], axis=1
             )
+            for i, snap in enumerate(range(constants.MIN_SNAP, 100, 1)):
+                if log:
+                    q = np.log10(quantity[snap])
+                else:
+                    q = quantity[snap]
+                quantity_hists[zoom_id, i] = np.histogram(
+                    q, self.n_bins, range=hist_range
+                )[0]
 
         # Step 4: plot line plots
         self._plot_and_save_lineplots(
             quantity_mean, quantity_median, quantity_min, quantity_max
         )
 
+        if hist_range is None:
+            logging.warning(
+                "Plot config was incomplete, cannot plot ridgeline and 2D "
+                "histogram plots."
+            )
+            return 0
+
         # Step 5: plot 2D histograms
-        self._plot_and_save_2dhistograms()
+        self._plot_and_save_2dhistograms(quantity_hists)
 
         # Step 6: plot ridgeline plots
-        self._plot_and_save_ridgelineplots()
+        self._plot_and_save_ridgelineplots(
+            quantity_hists, hist_range[0], hist_range[1]
+        )
 
         return 0
 
@@ -156,10 +198,57 @@ class PlotSimpleQuantityWithTimePipeline(base.Pipeline):
                 fig, ident_flag=label_prefix.lower(), subdir="./1d_plots"
             )
 
-    def _plot_and_save_ridgelineplots(self):
-        # TODO: implement
-        pass
+    def _plot_and_save_ridgelineplots(
+        self, hists: NDArray, minimum: float, maximum: float
+    ) -> None:
+        """
+        Plot a ridgeline plot of the development of the quantity.
 
-    def _plot_and_save_2dhistograms(self):
+        Plot shows the 1D distribution of the quantity at different
+        redshifts. It plots the mean and median distribution over all
+        clusters in every snapshot.
+
+        :param hists: Array of histograms, of shape (352, 92, N) where
+            N is the number of bins.
+        :param minimum: Leftmost edge of the histogram bins.
+        :param maximum: Rightmost edge of the histogram bins.
+        :return: None, figure saved to file.
+        """
+        # color map
+        cmap = matplotlib.cm.get_cmap("gist_heat")
+        norm = matplotlib.colors.Normalize(vmin=constants.MIN_SNAP, vmax=120)
+
+        for method in ["mean", "median"]:
+            # Step 1: stack histograms
+            stacked_hist = statistics.stack_histograms(
+                hists, method, axis=0
+            )[0]
+
+            # Step 2: set up figure
+            fig, axes = plt.subplots(figsize=(5, 5))
+            q_label = self.quantity_label[0].lower() + self.quantity_label[1:]
+            axes.set_xlabel(f"{method.capitalize()} {q_label}")
+            axes.set_ylabel("Snap num")
+
+            # Step 3: set up x-values
+            xs = np.linspace(minimum, maximum, num=self.n_bins)
+
+            # Step 4: set up y-values and y-axis ticks and labels
+            y_base = np.arange(constants.MIN_SNAP, 100, step=1)
+
+            # Step 5: plot baselines
+            axes.hlines(y_base, minimum, maximum, color="grey", linewidths=1)
+
+            # Step 6: plot ridgelines
+            for i in range(self.n_snaps):
+                ys = y_base[i] + stacked_hist[i] / np.sum(stacked_hist[i]) * 30
+                color = cmap(norm(i + constants.MIN_SNAP))
+                axes.plot(xs, ys, color=color)
+
+            # Step 6: save figure
+            self._save_fig(fig, ident_flag=method, subdir="2d_plots")
+            logging.info(f"Saved {method} ridgeline plot to file.")
+
+    def _plot_and_save_2dhistograms(self, hists: NDArray) -> None:
         # TODO: implement
         pass
