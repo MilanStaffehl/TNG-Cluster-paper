Fix distance computation function
Function would give wrong result for distances larger than half the
box size but smaller than the full box size. It now implicitly
assumes that distances can never surpass full box size, i.e. that
all coordinates are from box domain.

src/library/compute.py
@@ -181,6 +181,12 @@ def get_distance_periodic_box(
     direction to half the box size before finding the norm of the
     distance vector between points A and B.
 
+    .. attention:: This function implicitly assumes that all coordinates
+        are limited to the box domain, i.e. it assumes that no distance
+        between two points can be larger than the box size itself. If
+        you have coordinates that can lie outside the box volume, they
+        must be normalized to the box volume first!
+
     :param positions_a: Either an array of shape (N, 3) or a vector of
         shape (3, ).
     :param positions_b: Either an array of shape (N, 3) or a vector of
@@ -200,10 +206,8 @@ def get_distance_periodic_box(
     half_box_size = 0.5 * box_size
     d = positions_a - positions_b
     # limit to box size
-    pos_multiple = np.floor(d[d > half_box_size] / box_size)
-    d[d > half_box_size] -= box_size * pos_multiple
-    neg_multiple = np.floor(d[d < -half_box_size] / -box_size)
-    d[d < -half_box_size] += box_size * neg_multiple
+    d[d > half_box_size] -= box_size
+    d[d < -half_box_size] += box_size
 
     if len(d.shape) > 1:
         # list of vectors

src/library/tests/test_compute.py
@@ -127,6 +127,16 @@ def test_get_distance_periodic_box_surpassed_distance():
     assert output == 1.5
 
 
+def test_get_distance_periodic_box_barely_surpassed_distance():
+    """Test function, surpassing box length just slightly"""
+    a = np.array([1, 0, -1.5], dtype=float)
+    b = np.array([0, 1, 0], dtype=float)
+    output = compute.get_distance_periodic_box(a, b, 2)
+    assert isinstance(output, float)
+    assert output == 1.5
+
+
+@pytest.mark.skip(reason="Multiples of box size not supported.")
 def test_get_distance_periodic_box_surpassed_multiple():
     """Test function for multiple box lengths too many"""
     a = np.array([1, 0, -1], dtype=float)
@@ -139,11 +149,11 @@ def test_get_distance_periodic_box_surpassed_multiple():
 def test_get_distance_periodic_box_array():
     """Test the function with an array of vectors"""
     a = np.array(
-        [[2, 1, 0], [-1, 0, 1], [0, 0, 0], [2, -2, 2]],
+        [[1, 1, 0], [-1, 0, 1], [0, 0, 0], [2, -2, 2]],
         dtype=float,
     )
     b = np.array(
-        [[-2, 1, -1], [0, 2, 2], [-1, 1, -1], [0, 0, 0]],
+        [[-1, 1, -1], [0, 2, 2], [-1, 1, -1], [0, 0, 0]],
         dtype=float,
     )
     expected = np.array([1, np.sqrt(2), np.sqrt(3), 0], dtype=float)