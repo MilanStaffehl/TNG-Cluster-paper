f4a7387fc9cf458198774f83b53669d3260948cd
Milan Staffehl <milan.staffehl@stud.uni-heidelberg.de>
2024-09-16 16:08:09 +0200

    Update batch script for distance pipeline

--- scripts/tracer_history/batch/generate_particle_data_distance_to_main_progenitor.sh
+++ scripts/tracer_history/batch/generate_particle_data_distance_to_main_progenitor.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=PD-DMP                     # Main job name for tracking
-#SBATCH --time=06:00:00                       # Total time limit per job (6 hours)
+#SBATCH --job-name=DIST                       # Main job name for tracking
+#SBATCH --time=26:00:00                       # Total time limit per job (6 hours)
 #SBATCH --partition=p.large                   # Specify your cluster partition (adjust as needed)
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=72
-#SBATCH --array=0-351%32
 #SBATCH -D ./
-#SBATCH -o /vera/u/mista/thesisProject/scripts/tracer_history/batch/output/generate_distance_to_mp_z%a.%A.out
-#SBATCH -e /vera/u/mista/thesisProject/scripts/tracer_history/batch/output/generate_distance_to_mp_z%a.%A.err
+#SBATCH -o /vera/u/mista/thesisProject/scripts/tracer_history/batch/output/generate_distance_to_mp.%j.out
+#SBATCH -e /vera/u/mista/thesisProject/scripts/tracer_history/batch/output/generate_distance_to_mp.%j.err
 
 module purge
 module load gcc/13 impi/2021.9
@@ -19,4 +18,4 @@ source ~/venvs/illustris/bin/activate
 which python3
 
 # Each srun command launches a job with specific resource requests
-srun python3 ~/thesisProject/scripts/tracer_history/plot_quantity_with_time.py distance -p "$SLURM_CPUS_PER_TASK" -u -fo -x -f -z "$SLURM_ARRAY_TASK_ID"
+srun python3 ~/thesisProject/scripts/tracer_history/plot_quantity_with_time.py distance -p "$SLURM_CPUS_PER_TASK" -u -fo -x -f
