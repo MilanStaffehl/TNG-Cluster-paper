c72d0443183ca88ea7f270fc73b46fc586f49b6e
Milan Staffehl <milan.staffehl@stud.uni-heidelberg.de>
2024-10-17 15:48:55 +0200

    Add method to plot cumulative fraction of tracers in satellites

--- src/pipelines/tracer_history/parent_categories.py
+++ src/pipelines/tracer_history/parent_categories.py
@@ -8,8 +8,6 @@ import logging
 from typing import TYPE_CHECKING, ClassVar
 
 import h5py
-import matplotlib.cm
-import matplotlib.colors
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -66,9 +64,13 @@ class PlotParentCategoryPlots(base.Pipeline):
         logging.info("Plotting fraction of tracers in other halos.")
         self._plot_category_fractions(1, "other halos", masses, archive_file)
 
-        # Step 4: plot fraction of tracers in other halos
+        # Step 5: plot fraction of tracers in other halos
         logging.info("Plotting fraction of unbound tracers.")
         self._plot_category_fractions(0, "no host", masses, archive_file)
+
+        # Step 6: plot cumulative fraction of tracers in satellites
+        logging.info("Plotting cumulative fraction of tracers in satellites.")
+        self._plot_cumulative_satellite_fraction(masses, archive_file)
 
         return 0
 
@@ -107,41 +109,62 @@ class PlotParentCategoryPlots(base.Pipeline):
         axes.set_ylabel(f"Fraction of tracers in {category_name}")
         xs = common.make_redshift_plot(axes, start=constants.MIN_SNAP)
 
-        # Step 4: create colorscale for lines
-        cmap = matplotlib.cm.get_cmap("plasma")
-        norm = matplotlib.colors.Normalize(vmin=14.0, vmax=15.4)
-        colors = [cmap(norm(mass)) for mass in masses]
+        # Step 4: plot lines and mean/median
+        common.plot_cluster_line_plot(fig, axes, xs, current_fraction, masses)
 
-        # Step 5: plot the datapoints
-        plot_config = {
-            "marker": "none",
-            "linestyle": "solid",
-            "alpha": 0.1,
-        }
-        for i in range(self.n_clusters):
-            axes.plot(
-                xs,
-                current_fraction[i],
-                color=colors[i],
-                **plot_config,
-            )
-        fig.colorbar(
-            matplotlib.cm.ScalarMappable(norm=norm, cmap=cmap),
-            ax=axes,
-            location="right",
-            label="$log_{10} M_{200c}$ at z = 0",
-        )
-
-        # Step 6: plot mean and median
-        m_config = {"marker": "none", "color": "black"}
-        mean = np.nanmean(current_fraction, axis=0)
-        axes.plot(xs, mean, ls="solid", **m_config)
-        median = np.nanmedian(current_fraction, axis=0)
-        axes.plot(xs, median, ls="dashed", **m_config)
-
-        # Step 7: save figure
+        # Step 5: save figure
         category_file = category_name.replace(" ", "_")
         self._save_fig(fig, ident_flag=f"current_{category_file}_fraction")
         logging.info(
             f"Finished saving plot for {category_name} fraction to file."
+        )
+
+    def _plot_cumulative_satellite_fraction(
+        self, masses: NDArray, archive_file: h5py.File
+    ) -> None:
+        """
+        Plot the fraction of tracers that have ever been in satellites.
+
+        :param masses: The cluster masses in log scale.
+        :param archive_file: The opened cool gas archive file.
+        :return: None, figure is saved to file.
+        """
+        # Step 1: find fraction of tracers that have ever been in a satellite
+        cum_fractions = np.empty((self.n_clusters, self.n_snaps))
+        for zoom_in in range(self.n_clusters):
+            # Step 2.1: get parent categories
+            grp = f"ZoomRegion_{zoom_in:03d}"
+            categories = archive_file[grp]["ParentCategory"][()]
+            n_particles = categories.shape[1]
+
+            # Step 2.2: find the first time that every particle is in a
+            # satellite using argmax on a boolean array
+            first_snap_in_satellite = np.argmax(categories == 4, axis=0)
+            # Set all zeros to a high value, so we don't accidentally count
+            # those entries that are never in a satellite, which argmax sets
+            # to zero, as "has been in a satellite for the first time at snap
+            # zero". We can get away with this, since we KNOW that not a single
+            # particle can be category 4 at snapshot zero: everything before
+            # MIN_SNAP is set to category 255, always and everywhere.
+            first_snap_in_satellite[first_snap_in_satellite == 0] = 101
+            assert np.all(first_snap_in_satellite >= constants.MIN_SNAP)
+            for snap_num in range(constants.MIN_SNAP, 100):
+                i = snap_num - constants.MIN_SNAP
+                n_ever_in_satellite = np.count_nonzero(
+                    first_snap_in_satellite <= snap_num
+                )
+                cum_fractions[zoom_in][i] = n_ever_in_satellite / n_particles
+
+        # Step 2: create a figure
+        fig, axes = plt.subplots(figsize=(5, 4))
+        xs = common.make_redshift_plot(axes, start=constants.MIN_SNAP)
+        axes.set_ylabel("Cum. fraction of tracers in satellites")
+
+        # Step 3: plot the fraction
+        common.plot_cluster_line_plot(fig, axes, xs, cum_fractions, masses)
+
+        # Step 4: save plot to file
+        self._save_fig(fig, ident_flag="cumulative_satellite_frac")
+        logging.info(
+            "Finished saving plot for cumulative satellite fraction to file."
         )
