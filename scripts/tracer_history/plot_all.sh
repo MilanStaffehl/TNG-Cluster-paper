7c67be022dab0a361cee490bf3d5533876a1ade8
Milan Staffehl <milan.staffehl@stud.uni-heidelberg.de>
2024-11-07 17:03:28 +0100

    Update `plot_all.sh`

--- scripts/tracer_history/plot_all.sh
+++ scripts/tracer_history/plot_all.sh
@@ -1,90 +1,92 @@
 #!/bin/bash
 # STACKS PHYSICAL UNITS
-printf "Running: distance plots (stacks, all plot types)"
-python ./plot_quantity_with_time.py distance -l
-printf "Running: volume normalized distance plots (stacks, all plot types)"
-python ./plot_quantity_with_time.py distance -l -vn
-printf "Running: temperature plots (stacks, all plot types)"
-python ./plot_quantity_with_time.py temperature -l
-printf "Running: density plots (stacks, all plot types)"
-python ./plot_quantity_with_time.py density -l
+printf "\n\nRunning: distance plots (stacks, all plot types)"
+python ./plot_quantity_with_time.py distance -l -q
+printf "\n\nRunning: volume normalized distance plots (stacks, all plot types)"
+python ./plot_quantity_with_time.py distance -l -vn -q
+printf "\n\nRunning: temperature plots (stacks, all plot types)"
+python ./plot_quantity_with_time.py temperature -l -q
+printf "\n\nRunning: density plots (stacks, all plot types)"
+python ./plot_quantity_with_time.py density -l -q
 
 # STACKS NORMALIZED
-printf "Running: distance plots (normalized stacks, all plot types)"
-python ./plot_quantity_with_time.py distance -l -n
-printf "Running: volume normalized distance plots (normalized stacks, all plot types)"
-python ./plot_quantity_with_time.py distance -l -vn -n
-printf "Running: temperature plots (normalized stacks, all plot types)"
-python ./plot_quantity_with_time.py temperature -l -n
+printf "\n\nRunning: distance plots (normalized stacks, all plot types)"
+python ./plot_quantity_with_time.py distance -l -n -q
+printf "\n\nRunning: volume normalized distance plots (normalized stacks, all plot types)"
+python ./plot_quantity_with_time.py distance -l -vn -n -q
+printf "\n\nRunning: temperature plots (normalized stacks, all plot types)"
+python ./plot_quantity_with_time.py temperature -l -n -q
 
 # STACKS SPLIT BY PARENT
-printf "Running: distance plots (stacks, split by parent, all plot types)"
-python ./plot_quantity_with_time.py distance -l -pt 1 --split-by parent-category
-printf "Running: volume normalized distance plots (stacks, split by parent, all plot types)"
-python ./plot_quantity_with_time.py distance -l -vn -pt 1 --split-by parent-category
-printf "Running: temperature plots (stacks, split by parent, all plot types)"
-python ./plot_quantity_with_time.py temperature -l -pt 1 --split-by parent-category
-printf "Running: density plots (stacks, split by parent, all plot types)"
-python ./plot_quantity_with_time.py density -l -pt 1 --split-by parent-category
+printf "\n\nRunning: distance plots (stacks, split by parent, all plot types)"
+python ./plot_quantity_with_time.py distance -l -pt 1 --split-by parent-category -q
+printf "\n\nRunning: volume normalized distance plots (stacks, split by parent, all plot types)"
+python ./plot_quantity_with_time.py distance -l -vn -pt 1 --split-by parent-category -q
+printf "\n\nRunning: temperature plots (stacks, split by parent, all plot types)"
+python ./plot_quantity_with_time.py temperature -l -pt 1 --split-by parent-category -q
+printf "\n\nRunning: density plots (stacks, split by parent, all plot types)"
+python ./plot_quantity_with_time.py density -l -pt 1 --split-by parent-category -q
 
 # STACKS SPLIT BY PARENT AT Z = 0
-printf "Running: distance plots (stacks, split by parent, all plot types)"
-python ./plot_quantity_with_time.py distance -l -pt 1 --split-by parent-category-at-zero
-printf "Running: volume normalized distance plots (stacks, split by parent, all plot types)"
-python ./plot_quantity_with_time.py distance -l -vn -pt 1 --split-by parent-category-at-zero
-printf "Running: temperature plots (stacks, split by parent, all plot types)"
-python ./plot_quantity_with_time.py temperature -l -pt 1 --split-by parent-category-at-zero
-printf "Running: density plots (stacks, split by parent, all plot types)"
-python ./plot_quantity_with_time.py density -l -pt 1 --split-by parent-category-at-zero
+printf "\n\nRunning: distance plots (stacks, split by parent, all plot types)"
+python ./plot_quantity_with_time.py distance -l -pt 1 --split-by parent-category-at-zero -q
+printf "\n\nRunning: volume normalized distance plots (stacks, split by parent, all plot types)"
+python ./plot_quantity_with_time.py distance -l -vn -pt 1 --split-by parent-category-at-zero -q
+printf "\n\nRunning: temperature plots (stacks, split by parent, all plot types)"
+python ./plot_quantity_with_time.py temperature -l -pt 1 --split-by parent-category-at-zero -q
+printf "\n\nRunning: density plots (stacks, split by parent, all plot types)"
+python ./plot_quantity_with_time.py density -l -pt 1 --split-by parent-category-at-zero -q
 
 # STACKS SPLIT BY BOUND STATE
-printf "Running: distance plots (stacks, split by bound state, all plot types)"
-python ./plot_quantity_with_time.py distance -l -pt 1 --split-by bound-state
-printf "Running: volume normalized distance plots (stacks, split by bound state, all plot types)"
-python ./plot_quantity_with_time.py distance -l -vn -pt 1 --split-by bound-state
-printf "Running: temperature plots (stacks, split by bound state, all plot types)"
-python ./plot_quantity_with_time.py temperature -l -pt 1 --split-by bound-state
-printf "Running: density plots (stacks, split by bound state, all plot types)"
-python ./plot_quantity_with_time.py density -l -pt 1 --split-by bound-state
+printf "\n\nRunning: distance plots (stacks, split by bound state, all plot types)"
+python ./plot_quantity_with_time.py distance -l -pt 1 --split-by bound-state -q
+printf "\n\nRunning: volume normalized distance plots (stacks, split by bound state, all plot types)"
+python ./plot_quantity_with_time.py distance -l -vn -pt 1 --split-by bound-state -q
+printf "\n\nRunning: temperature plots (stacks, split by bound state, all plot types)"
+python ./plot_quantity_with_time.py temperature -l -pt 1 --split-by bound-state -q
+printf "\n\nRunning: density plots (stacks, split by bound state, all plot types)"
+python ./plot_quantity_with_time.py density -l -pt 1 --split-by bound-state -q
 
 # STACKS SPLIT BY BOUND STATE AT Z = 0
-printf "Running: distance plots (stacks, split by bound state, all plot types)"
-python ./plot_quantity_with_time.py distance -l -pt 1 --split-by bound-state-at-zero
-printf "Running: volume normalized distance plots (stacks, split by bound state, all plot types)"
-python ./plot_quantity_with_time.py distance -l -vn -pt 1 --split-by bound-state-at-zero
-printf "Running: temperature plots (stacks, split by bound state, all plot types)"
-python ./plot_quantity_with_time.py temperature -l -pt 1 --split-by bound-state-at-zero
-printf "Running: density plots (stacks, split by bound state, all plot types)"
-python ./plot_quantity_with_time.py density -l -pt 1 --split-by bound-state-at-zero
+printf "\n\nRunning: distance plots (stacks, split by bound state, all plot types)"
+python ./plot_quantity_with_time.py distance -l -pt 1 --split-by bound-state-at-zero -q
+printf "\n\nRunning: volume normalized distance plots (stacks, split by bound state, all plot types)"
+python ./plot_quantity_with_time.py distance -l -vn -pt 1 --split-by bound-state-at-zero -q
+printf "\n\nRunning: temperature plots (stacks, split by bound state, all plot types)"
+python ./plot_quantity_with_time.py temperature -l -pt 1 --split-by bound-state-at-zero -q
+printf "\n\nRunning: density plots (stacks, split by bound state, all plot types)"
+python ./plot_quantity_with_time.py density -l -pt 1 --split-by bound-state-at-zero -q
 
 # INDIVIDUAL PLOTS
 for i in $(seq 0 351)
 do
   printf "\n\nRunning: distance plots (individual, zoom-in $i)\n"
-  python ./plot_quantity_with_time.py distance -l -z "$i"
+  python ./plot_quantity_with_time.py distance -l -z "$i" -q
   printf "\n\nRunning: volume normalized distance plots (individual, zoom-in $i)\n"
-  python ./plot_quantity_with_time.py distance -l -z "$i" -vn
+  python ./plot_quantity_with_time.py distance -l -z "$i" -vn -q
   printf "\n\nRunning: temperature plots (individual, zoom-in $i)\n"
-  python ./plot_quantity_with_time.py temperature -l -z "$i"
+  python ./plot_quantity_with_time.py temperature -l -z "$i" -q
   printf "\n\nRunning: density plots (individual, zoom-in $i)\n"
-  python ./plot_quantity_with_time.py density -l -z "$i"
+  python ./plot_quantity_with_time.py density -l -z "$i" -q
 done
 
 # INDIVIDUAL LINE PLOTS, SPLIT BY CATEGORY AT REDSHIFT ZERO
 for i in $(seq 0 351)
 do
   printf "\n\nRunning: distance line plots (individual, zoom-in $i, split by parent category)\n"
-  python ./plot_quantity_with_time.py distance -l -z "$i" -pt 0 --split-by parent-category-at-zero
+  python ./plot_quantity_with_time.py distance -l -z "$i" -pt 0 --split-by parent-category-at-zero -q
   printf "\n\nRunning: temperature line plots (individual, zoom-in $i, split by parent category)\n"
-  python ./plot_quantity_with_time.py temperature -l -z "$i" -pt 0 --split-by parent-category-at-zero
+  python ./plot_quantity_with_time.py temperature -l -z "$i" -pt 0 --split-by parent-category-at-zero -q
   printf "\n\nRunning: density line plots (individual, zoom-in $i, split by parent category)\n"
-  python ./plot_quantity_with_time.py density -l -z "$i" -pt 0 --split-by parent-category-at-zero
+  python ./plot_quantity_with_time.py density -l -z "$i" -pt 0 --split-by parent-category-at-zero -q
 done
 
 # CROSSING TIME PLOTS
 printf "\n\nRunning: crossing time plots (all plot types)"
-python ./plot_crossing_time_plots.py
+python ./plot_crossing_time_plots.py -q
 
 # PARENT CATEGORY PLOTS
 printf "\n\nRunning: parent category plots (all plot types)"
-python ./plot_parent_category_plots.py
+python ./plot_parent_category_plots.py -q
+
+printf "\n\nDone! Finished all plots."
